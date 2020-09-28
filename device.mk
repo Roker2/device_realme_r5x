4dc31e78ac187a48655f87d8cb6dcf15377dd5b6
Rahul Krishna <rahulkrishna585@gmail.com>
2020-09-28 22:38:40 +0530

    r5x: Build QTI thermal 2.0 hal
    Change-Id: I4d66a57a1fa162c1018ef5d13dee3346cf157a98
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -479,6 +479,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
