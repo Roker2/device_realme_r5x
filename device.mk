b2ac2b8d7c89ef9f2f824f35ee4ba900814024c8
Roker2 <dimaminko2000@gmail.com>
2024-04-11 23:02:04 +0600

    r5x: Fix build soundtrigger

--- device.mk
+++ device.mk
@@ -532,7 +532,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.radio@1.5.vendor \
     android.hardware.secure_element@1.2.vendor \
-    android.hardwareundtrigger@2.3.vendor \
+    android.hardware.soundtrigger@2.3.vendor \
     android.hardware.usb.gadget@1.1.vendor \
     android.hardware.usb@1.2.vendor \
     android.hardware.vibrator@1.3.vendor \
